2186e3c55e0dae7eabb8aaf408a86ac9796cbc4d
“Vince <vmemmo1@gmail.com>
2022-11-13 14:32:40 -0800

    express error handling

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 // backend/app.js
 
 const express = require("express");
-// const path = require('path'); REMOVED
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug')
 
-// const indexRouter = require('./routes/index'); REMOVED
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
 const usersRouter = require('./routes/api/users'); // update the import file path
 const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -17,9 +21,53 @@ app.use(express.urlencoded({ extended: false })); // parse urlencoded request bo
 app.use(cookieParser()); // parse cookies as an object on req.cookies
 //app.use(express.static(path.join(__dirname, 'public'))); REMOVED// serve the static files in the public folder
 
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+);
+  
+
 // Attach Express routers
 // app.use('/', indexRouter); REMOVED
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+  
+  const serverErrorLogger = debug('backend:error');
+  
+  // Express custom error handler that will be called whenever a route handler or
+  // middleware throws an error or invokes the `next` function with a truthy value
+  app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      statusCode,
+      errors: err.errors
+    })
+  });
 
 module.exports = app;
