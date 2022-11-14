1519f7071964dc17577f61b2f9940b1490983569
“Vince <vmemmo1@gmail.com>
2022-11-13 22:33:32 -0800

    frontend phase 5

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session';
+import errorsReducer from './errors';
 
 const rootReducer = combineReducers({
-  
+  session,
+  errors: errorsReducer
 });
 
 let enhancer;
