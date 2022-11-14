0286d18ce01ccb702a23cf9d97ebbcf840682cb2
“Vince <vmemmo1@gmail.com>
2022-11-13 18:50:35 -0800

    login functionality

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
-// backend/app.js
-
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const debug = require('debug')
 
 const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
-const usersRouter = require('./routes/api/users'); // update the import file path
+require('./models/User');
+require('./config/passport');
+
+const debug = require('debug');
+const passport = require('passport');
+
+const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
 
@@ -19,55 +22,56 @@ app.use(logger('dev')); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
-//app.use(express.static(path.join(__dirname, 'public'))); REMOVED// serve the static files in the public folder
 
+// Security Middleware
 if (!isProduction) {
-    // Enable CORS only in development because React will be on the React
-    // development server (http://localhost:3000). (In production, the Express 
-    // server will serve the React files statically.)
-    app.use(cors());
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, React files
+  // will be served statically on the Express server.)
+  app.use(cors());
 }
 
 // Set the _csrf token and create req.csrfToken method to generate a hashed
 // CSRF token
 app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
 );
-  
+
+// Passport initialization for authentication
+app.use(passport.initialize());
 
 // Attach Express routers
-// app.use('/', indexRouter); REMOVED
-app.use('/api/users', usersRouter); // update the path
+app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
 
 // Express custom middleware for catching all unmatched requests and formatting
 // a 404 error to be sent as the response.
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.statusCode = 404;
-    next(err);
-  });
-  
-  const serverErrorLogger = debug('backend:error');
-  
-  // Express custom error handler that will be called whenever a route handler or
-  // middleware throws an error or invokes the `next` function with a truthy value
-  app.use((err, req, res, next) => {
-    serverErrorLogger(err);
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode);
-    res.json({
-      message: err.message,
-      statusCode,
-      errors: err.errors
-    })
-  });
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 module.exports = app;
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-    username: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    hashedPassword: {
-      type: String,
-      required: true
-    }
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  }
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema);
