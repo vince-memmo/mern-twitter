19b8840c7d48b89838af6905dbde9e1ea387bfe8
“Vince <vmemmo1@gmail.com>
2022-11-13 20:21:13 -0800

    commit

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,21 +1,28 @@
+const bcrypt = require('bcryptjs');
+
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
-const passport = require('passport');
 const User = mongoose.model('User');
+const passport = require('passport');
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
 router.get('/', function(req, res, next) {
   res.json({
     message: "GET /api/users"
   });
 });
 
-// POST /api/users/register
-router.post('/register', async (req, res, next) => {
-  // Check to make sure no one has already registered with the proposed email or
-  // username.
+router.post('/register',validateRegisterInput, async (req, res, next) => {
+  // Your code will go here
   const user = await User.findOne({
     $or: [{ email: req.body.email }, { username: req.body.username }]
   });
@@ -25,19 +32,16 @@ router.post('/register', async (req, res, next) => {
     const err = new Error("Validation Error");
     err.statusCode = 400;
     const errors = {};
-
     if (user.email === req.body.email) {
       errors.email = "A user has already registered with this email";
     }
     if (user.username === req.body.username) {
       errors.username = "A user has already registered with this username";
     }
-
     err.errors = errors;
     return next(err);
   }
 
-  // Otherwise create a new user
   const newUser = new User({
     username: req.body.username,
     email: req.body.email
@@ -45,35 +49,49 @@ router.post('/register', async (req, res, next) => {
 
   bcrypt.genSalt(10, (err, salt) => {
     if (err) throw err;
-
     bcrypt.hash(req.body.password, salt, async (err, hashedPassword) => {
       if (err) throw err;
-
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user));
+        // return res.json({ user });
       }
       catch(err) {
         next(err);
       }
     })
   });
+
 });
 
-// POST /api/users/login
-router.post('/login', async (req, res, next) => {
+router.post('/login',validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
-    
     if (!user) {
       const err = new Error('Invalid credentials');
       err.statusCode = 400;
       err.errors = { email: "Invalid credentials" };
       return next(err);
     }
-    return res.json({ user });
+    return res.json(await loginUser(user));
   })(req, res, next);
+});
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
