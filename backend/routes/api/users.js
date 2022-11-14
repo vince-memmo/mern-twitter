d9a1d7ea6c197624e8a35b52e014cadd130d580d
“Vince <vmemmo1@gmail.com>
2022-11-13 22:26:17 -0800

    pushing all files

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/register', async (req, res, next) => {
+router.post('/register', validateRegisterInput, async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
   const user = await User.findOne({
